db125788a481112359caa95f624548f6ce88b208
Preetham Praveen <preethampraveencode26@gmail.com>
2025-02-24 11:44:18 +0530

    entities

--- src/components/bookcard.jsx
+++ src/components/bookcard.jsx
@@ -1,15 +1,22 @@
 // write the book component code here
 import React from "react";
+import '../App.css'
 
 function Bookcard({props}){
     return(
-        <>
-            {props.books.map((data)=>{
-                <div key={data.id}>
-                    <h1>data.name</h1>
+   
+        <div className="card">
+            {props.map((data)=>(
+                <div key={data.id} className="new">
+                    <img src={data.image}></img>
+                    <h2>{data.name}</h2>
+                    <h3>{data.genre}</h3>
+                    <h3>{data.author}</h3>
+                    
                 </div>
-            })}
-        </>
+            ))}
+            </div>
+   
     )
 }
 
