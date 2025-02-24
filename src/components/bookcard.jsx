3731730baec81155ac72f220d7a866d9278ea3fd
PREETH26 <preethampraveencode26@gmail.com>
2025-02-24 11:04:29 +0530

    new

--- src/components/bookcard.jsx
+++ src/components/bookcard.jsx
@@ -1,1 +1,16 @@
 // write the book component code here
+import React from "react";
+
+function Bookcard({props}){
+    return(
+        <>
+            {props.books.map((data)=>{
+                <div key={data.id}>
+                    <h1>data.name</h1>
+                </div>
+            })}
+        </>
+    )
+}
+
+export default Bookcard
